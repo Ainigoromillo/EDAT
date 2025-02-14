06dffce58a61f3bb3ace1e870b6dbd7832f4068e
MattTheCommiter <matteo.artu@gmail.com>
2025-02-14 13:06:04 +0100

    p1_e2.c

---
+++ p1_e2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "graph.h"
+#include "vertex.h"
+
+int main(){
+    Graph *g = graph_init();
+    long *array;
+    if(!graph_newVertex(g, "id:111 tag:Madrid state:WHITE")){
+        printf("Ha habido un error");
+    }
+    if(!(graph_newVertex(g, "id:222 tag:Toledo state:WHITE"))){
+        printf("Error");
+    }
+    graph_newEdge(g, 222, 1111);
+    printf("111 --> 222? ");
+    if(graph_connectionExists(g, 111, 222)){
+        printf("Yes");
+    }else{
+        printf("No");
+    }
+
+    printf("222 --> 111? ");
+    if(graph_connectionExists(g, 222, 111)){
+        printf("Yes");
+    }else{
+        printf("No");
+    }
+    printf("Number of connections from 111: %d", graph_getNumberOfConnectionsFromId(g, 111));
+    printf("Number of connections from Toledo: %d", graph_getNumberOfConnectionsFromTag(g, "Toledo"));
+    array=graph_getConnectionsFromTag(g, "Toledo");
+    for(int i=0;i<graph_getNumberOfConnectionsFromTag(g, "Toledo"); i++){
+        printf("%ld", array[i]);
+    }
+    graph_print(stdout, g);
+    graph_free(g);
+}
